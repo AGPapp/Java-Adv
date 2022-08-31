aa41fda711d6b376e62bbe1ee1dab012011386c3
Andrey <agpopov@appline.ru>
2022-08-31 19:23:32 +0300

    Перенес отлов исключений

--- src/NewCalc/Calc.java
+++ src/NewCalc/Calc.java
@@ -1,11 +1,12 @@
 package NewCalc;
 
+import java.util.InputMismatchException;
+
 public class Calc {
     public static double answer;
     private double x;
     private double y;
     private String o;
-
     public Calc(double x, double y, String o) {
         this.x = x;
         this.y = y;
@@ -13,38 +14,27 @@ public class Calc {
     }
 
     public double Calculator() {
-        try {
-            switch (o) {
-                case "+":
-                    answer = x + y;
-                    break;
-                case "-":
-                    answer = x - y;
-                    break;
-                case "*":
-                    answer = x * y;
-                    break;
-                case "/":
-                    try {
-                        if (y == 0) {
-                            throw new ArithmeticException();
-                        }
-                        else
-                        answer = x / y;
-                    } catch (ArithmeticException myErr) {
-                        System.err.println("Деление на ноль недоступно");
-                        System.exit(1);
-                    }
-                    break;
-                default: throw new Exception();
-            }
-        } catch (Exception err){
-            System.err.println("Неизвестный оператор");
-            System.exit(2);
-        }
 
-            return answer;
+        switch (o) {
+            case "+":
+                answer = x +y;
+                break;
+            case "-":
+                answer = x-y;
+                break;
+            case "*":
+                answer = x*y;
+                break;
+            case "/":
+                if (y == 0) {throw new ArithmeticException();
+                }
+                else answer = x / y;
+
+            default: throw new InputMismatchException() ;
+
         }
+        return answer;
+    }
 
     }
 
--- src/NewCalc/Main.java
+++ src/NewCalc/Main.java
@@ -7,34 +7,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите первое число: ");
-        double x=0;
+        double x = 0;
+        double y = 0;
         try {
             x = in.nextDouble();
-        } catch (InputMismatchException e) {
-            System.err.println("Ошибка: введено не число!");
-            System.exit(0);
-        }
+
         System.out.println("Введите второе число: ");
-        double y = 0;
-                try{
-                    y = in.nextDouble();
+
+
+        y = in.nextDouble();
                 } catch (InputMismatchException e){
                     System.err.println("Ошибка: введено не число!");
-                System.exit(0);}
+                System.exit(2);}
 
         System.out.println("Введите оператор: +, -, *, / :" );
         String o = in.next();
-
-        Calc calc = new Calc(x, y, o);
-        calc.Calculator();
         in.close();
+        try {
+            Calc calc = new Calc(x, y, o);
+            calc.Calculator();
+        } catch(ArithmeticException p){
+            System.err.println("Произошло деление на 0, но так как числа не целые, процесс не останавливаем");
+          // System.exit(1);
+        }
+        catch (InputMismatchException l){
+            System.err.println("Неизвестный оператор");
+            System.exit(1);
+        }
 
         double answer = Calc.answer;
         System.out.println("Результат:" + answer);
 
     }
-
-
 
 }
 
